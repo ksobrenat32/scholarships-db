4783863dcddeb8fbc95762636e75b0f1a3aa8ca4
ksobrenat32 <ksobrenatural@tuta.io>
2025-01-29 20:10:05 -0600

    Force every user to have a trabajador linked

--- main/views.py
+++ main/views.py
@@ -5,11 +5,24 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from django.contrib.auth.decorators import login_required
 from main.forms import TrabajadorCreateForm, BecarioCreateForm
+from main.models import Trabajador
 from django.http import HttpResponseForbidden, FileResponse
 from django.conf import settings
 import os
+import re
 
 # Create your views here.
+
+# Middleware to check if user has a linked trabajador
+def trabajador_required(view_func):
+    def _wrapped_view_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            try:
+                request.user.trabajador
+            except Trabajador.DoesNotExist:
+                return redirect('create_trabajador')
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view_func
 
 # Home view
 def home(request):
@@ -20,6 +33,13 @@ def signup(request):
     if request.method == 'GET':
         return render(request, 'signup.html', {'form': UserCreationForm()})
     elif request.method == 'POST':
+        curp = request.POST['username']
+        curp_regex = re.compile(r'^([A-Z]{4}\d{6}[HM][A-Z]{5}[A-Z0-9]{2})$')
+        if not curp_regex.match(curp):
+            return render(request, 'signup.html', {
+                'form': UserCreationForm(),
+                'error': 'Formato de CURP inválido'
+            })
         if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.create_user(
@@ -68,6 +88,7 @@ def signin(request):
 
 # Becas view
 @login_required
+@trabajador_required
 def becas(request):
     return render(request, 'becas.html')
 
@@ -106,6 +127,7 @@ def create_trabajador(request):
 
 # Create_Becario view
 @login_required
+@trabajador_required
 def create_becario(request):
     if request.method == 'GET':
         return render(request, 'create_becario.html', {'form': BecarioCreateForm()})
@@ -139,6 +161,7 @@ def create_becario(request):
 
 # File download view
 @login_required
+@trabajador_required
 def download_file(request, file_path):
     if not request.user.is_staff:
         return HttpResponseForbidden("You do not have permission to access this file.")
