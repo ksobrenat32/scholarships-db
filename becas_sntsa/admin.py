5cb339d1a4cdb9ce90f44404b9ea153315ed697b
ksobrenat32 <ksobrenatural@tuta.io>
2025-07-20 12:46:51 -0600

    Show the trabajador in the solicitud inside the admin panel

--- becas_sntsa/admin.py
+++ becas_sntsa/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from becas_sntsa.models import Seccion, Puesto, LugarAdscripcion, Grado, Trabajador, Becario, SolicitudAprovechamiento, SolicitudExcelencia, SolicitudEspecial
 
 # Register your models here.
@@ -16,14 +18,29 @@ class TrabajadorAdmin(admin.ModelAdmin):
 class BecarioAdmin(admin.ModelAdmin):
     search_fields = ['curp']  # Search by curp
 
-class SolicitudAprovechamientoAdmin(admin.ModelAdmin):
+class SolicitudAdmin(admin.ModelAdmin):
     search_fields = ['becario__curp', 'fecha_solicitud', 'becario__trabajador__username']
+    list_display = ('__str__', 'get_trabajador')
+    readonly_fields = ('get_trabajador',)
 
-class SolicitudExcelenciaAdmin(admin.ModelAdmin):
-    search_fields = ['becario__curp', 'fecha_solicitud', 'becario__trabajador__username']
+    def get_trabajador(self, obj):
+        trabajador_user = obj.becario.trabajador
+        try:
+            trabajador = Trabajador.objects.get(usuario=trabajador_user)
+            url = reverse('admin:becas_sntsa_trabajador_change', args=[trabajador.pk])
+            return format_html('<a href="{}">{}</a>', url, trabajador_user.username)
+        except Trabajador.DoesNotExist:
+            return trabajador_user.username
+    get_trabajador.short_description = 'Trabajador'
 
-class SolicitudEspecialAdmin(admin.ModelAdmin):
-    search_fields = ['becario__curp', 'fecha_solicitud', 'becario__trabajador__username']
+class SolicitudAprovechamientoAdmin(SolicitudAdmin):
+    pass
+
+class SolicitudExcelenciaAdmin(SolicitudAdmin):
+    pass
+
+class SolicitudEspecialAdmin(SolicitudAdmin):
+    pass
 
 admin.site.register(Trabajador, TrabajadorAdmin)
 admin.site.register(Becario, BecarioAdmin)
